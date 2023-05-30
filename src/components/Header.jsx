13075a341d7ea8bb5c728abaac79b936523cf9dd
kssfsamayasanjeevinifoundation <kssfsamayasanjeevinifoundation@gmail.com>
2023-05-30 18:31:41 +0530

    this new

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+        <div className="header">
+        <h1>  KSSF SAMAYA SANJEEVINI FOUNDATION</h1>
+        <h3>Give a Blood once in a lifetime <br /> That can save a life!!!.</h3>
+        <img width={'100px'} src="https://static.vecteezy.com/system/resources/previews/007/063/118/original/blood-drop-isolated-on-white-background-free-vector.jpg" alt="" />
+      </div>
+  )
+}
